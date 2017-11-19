eaed706aad5f9e85f0ddae5153e370d39adc5888
Natuchis <nataliagbar@gmail.com>
2017-11-19 17:37:50 -0300

    Add Ej1: plot the three signals

---
+++ natiTP/Ej1.m
@@ -0,0 +1,13 @@
+function Ej1()
+
+load('../TPSyS2017-2c-ARCHIVOS/intensidad_RGB.mat');
+colCount = size(brillo, 1);
+
+X = (0:1:(colCount-1))/FR;
+plot(X, brillo(:, 1), 'r', X, brillo(:, 2), 'g', X, brillo(:, 3), 'b');
+
+title('Intensidad de canales RGB')
+xlabel('Tiempo [s]')
+ylabel('Intensidad [???]')
+
+%print('IntensidadRGB','-dpng')
