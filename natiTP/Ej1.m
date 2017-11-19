6b09c402d23a0490fa5411f861a86e8cf11d37d5
Natuchis <nataliagbar@gmail.com>
2017-11-19 20:19:38 -0300

    Ej1: change in title and create png

--- natiTP/Ej1.m
+++ natiTP/Ej1.m
@@ -6,8 +6,8 @@ colCount = size(brillo, 1);
 X = (0:1:(colCount-1))/FR;
 plot(X, brillo(:, 1), 'r', X, brillo(:, 2), 'g', X, brillo(:, 3), 'b');
 
-title('Intensidad de canales RGB')
+title('Intensidad de la señal RGB')
 xlabel('Tiempo [s]')
 ylabel('Intensidad [???]')
 
-%print('IntensidadRGB','-dpng')
+print('Ej1 - IntensidadRGB','-dpng')
