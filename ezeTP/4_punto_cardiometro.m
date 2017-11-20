854fba2c1e5da3ca46ffe955f714fd055e54b0ae
egarcia91 <garciaezequiel91@gmail.com>
2017-11-20 17:38:16 -0300

    va el pdf

--- ezeTP/4_punto_cardiometro.m
+++ ezeTP/4_punto_cardiometro.m
@@ -1,5 +1,6 @@
 clear all;
 
+pkg load control;
 addpath('/usr/share/octave/packages/signal-1.3.2'); %incluye el specgram
 
 load intensidad_RGB.mat; % brillo, 1 es R, 2 es G, 3 es B.
@@ -13,8 +14,36 @@ ejeXvideo = 0 : t_video : ((video_length-1)*t_video);
 
 [b, a] = butter( 4, [0.4*2/frec_video , 10*2/frec_video]); %FiltroPincripal
 
+%Diagrama polos y ceros
+figure;
+pzmap(tf(a,b));
+print -djpg imagenes/punto_4_diagrama_polos_ceros_cardiometro.jpg; %Octave
+grid minor;
+
 %Disenia el filtro para una senial compuesta por un uno y todo los demas ceros escalado por Fs
 figure;
 freqz(b, a, 512, "whole", frec_video);
 print -djpg imagenes/punto_4_diagrama_filtro_cardiometro.jpg; %Octave
 grid minor;
+
+f = linspace(-frec_video/2, frec_video/2, video_length);
+
+impulso = zeros(1,240);
+impulso(1) = 1;
+
+respuestaImpulso = filtfilt(b, a, impulso);
+respuestaImpulso(video_length) = 0;
+
+DFT_respuestaImpulso = 20*log(fftshift(abs(fft(respuestaImpulso))));
+
+figure;
+
+plot( f, DFT_respuestaImpulso, 'b');
+xlabel('f [Hz]');
+ylabel('Modulo [dB]');
+title('FFT del filtro');
+
+print -djpg imagenes/punto_4_respuesta_impulso_cardiometro.jpg; %Octave
+grid minor;
+
+
--- ezeTP/5_punto_cadiometro.m
+++ ezeTP/5_punto_cadiometro.m
@@ -17,17 +17,33 @@ filtrado = filter(b, a, brillo(:,2));
 
 figure;
 
-subplot(2,1,1);
 plot( ejeXvideo, brillo(:, 2), 'g');
 hold on;
 plot(ejeXvideo, filtrado);
-title('Senial filtrada');
+%title('Senial filtrada');
 ylim([-1e+07 1e+07]);
+xlabel('t [s]');
+ylabel('Brillo');
 
-subplot(2,1,2);
+print -djpg imagenes/punto_5_superposicion_filtro_cardiometro.jpg; %Octave
+grid minor;
+
+figure;
+subplot(3,1,1);
 plot(ejeXvideo, filtrado);
 ylim([-1e+07 1e+07]);
-xlim([20 60]);
+xlim([3 30]);
+
+subplot(3,1,2);
+plot(ejeXvideo, filtrado);
+ylim([-1e+07 1e+07]);
+xlim([30 60]);
+
+subplot(3,1,3);
+plot(ejeXvideo, filtrado);
+ylim([-1e+07 1e+07]);
+xlim([60 90]);
+xlabel('t [s]');
 
 print -djpg imagenes/punto_5_filtro_cardiometro.jpg; %Octave
 grid minor;
